e3361a89058f4bfdbd09f78f1b44c46b8f9e8efd
Paulo Rodriguez Contreras <prodriguezc@unsa.edu.pe>
2020-07-20 14:07:53 -0500

    Project creation and giving permissions

---
+++ Lab11_RecordingAudio/app/src/main/java/com/dnpa/lab11/recordingaudio/RecordingAudioActivity.java
@@ -0,0 +1,14 @@
+package com.dnpa.lab11.recordingaudio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecordingAudioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
